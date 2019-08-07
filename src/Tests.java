09bb49707803837f15dfb7bb7fc3d8bdd6b00060
Emil John  D. Lopez <11816600@dlsu.edu.ph>
2019-08-07 19:09:44 +0800

    Added tester files

--- src/Tests.java
+++ src/Tests.java
@@ -1,3 +1,28 @@
-public class Test {
-    
+import Server.Structures.*;
+import Server.Transaction.UserDatabase;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Tests {
+    @Test
+    public void xmlParseTest(){
+
+        UserDatabase testDatabase = new UserDatabase("users.txt");
+        try {
+            StructureParser testParse = new StructureParser("test.xml", testDatabase);
+            ArrayList<Building> result = testParse.parse();
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
