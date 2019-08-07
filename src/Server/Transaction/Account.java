92966085daa1e3bcfdaa7fccce58a905ff7f1b61
Emil John  D. Lopez <11816600@dlsu.edu.ph>
2019-08-07 17:49:55 +0800

    Added passive mode

--- src/Server/Transaction/Account.java
+++ src/Server/Transaction/Account.java
@@ -1,17 +1,31 @@
 package Server.Transaction;
 
 public class Account {
-    String user;
+    private String user;
+    private boolean passiveState;
+
+    static public Account pseudoLogin(String _user, UserDatabase data){
+        if (data.presentInDataBase(_user)){
+            return new Account(_user, true);
+        }
+        else return null;
+    }
 
     static public Account login(String _user,
                                 String password,
                                 UserDatabase data){
         if (data.inDatabase(_user, password))
-            return new Account(_user);
+            return new Account(_user, false);
         else return null;
     }
 
-    protected Account(String _user){
+    protected Account(String _user, boolean isPassive){
         this.user = _user;
+        passiveState = isPassive;
+    }
+    //security-protocol
+    public boolean isPassive(){
+        return passiveState;
     }
 }
+
