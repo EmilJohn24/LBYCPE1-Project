0cba76b6c9c50be55bf2c78f070e834382015aa6
Emil John Lopez <emiljohn24@gmail.com>
2019-08-10 22:04:11 +0800

    Added static session adders

--- src/Server/Transaction/SessionManager.java
+++ src/Server/Transaction/SessionManager.java
@@ -4,17 +4,37 @@ import javax.security.auth.login.FailedLoginException;
 import java.util.HashMap;
 
 public class SessionManager {
-    private static HashMap<Integer, Account> sessionID; //links session IDs to accounts
+    private static HashMap<Integer, Account> sessions; //links session IDs to accounts
     private static UserDatabase users;
-    public static Integer login(String username, String password) throws FailedLoginException {
+
+    public static Integer getHashCode(Account acc){
+        return acc.getUsername().hashCode();
+    }
+    public static Account login(String username, String password) throws FailedLoginException {
         Account newLogin = Account.login(username, password, users);
         if (newLogin != null) {
-            Integer id = newLogin.getUsername().hashCode();
-            return id;
+            return newLogin;
         }
         else{
             throw new FailedLoginException("Login failed. Invalid username or password");
         }
     }
+
+    private static Integer addToSessions(Account acc) {
+        Integer hash = getHashCode(acc);
+        sessions.put(hash, acc);
+        return hash;
+    }
+
+    public static Integer addSession(Account newAccount){
+        return addToSessions(newAccount);
+    }
+
+    public static Integer addSession(String user, String password) throws FailedLoginException {
+        return addToSessions(SessionManager.login(user, password));
+    }
+
+
+
 
 }
