0311cae6a2162b47a8b5c7e4f068551b107e5e57
Emil John Lopez <emiljohn24@gmail.com>
2019-08-11 02:40:46 +0800

    Structure parser rename

--- src/Server/Transaction/Manager.java
+++ src/Server/Transaction/Manager.java
@@ -1,13 +1,13 @@
 package Server.Transaction;
 
-import Server.Structures.StructureParser;
+import Server.Structures.StructureHandler;
 
 //central manager of every operation
 //will be set as a static class
 public class Manager {
 
     private static UserDatabase database;
-    private static StructureParser parser;
+    private static StructureHandler parser;
     public Account getAccountLogin(String username,
                                    String password){
         return Account.login(username, password, database);
