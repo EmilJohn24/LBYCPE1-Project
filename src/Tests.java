b7315f09e79fd3423022ee0e92a0c10ba4b4a348
Emil John Lopez <emiljohn24@gmail.com>
2019-08-11 02:41:30 +0800

    Finished tests

--- src/Tests.java
+++ src/Tests.java
@@ -1,9 +1,12 @@
 import Server.Structures.*;
+import Server.Transaction.Account;
 import Server.Transaction.UserDatabase;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -12,8 +15,10 @@ public class Tests {
     public void xmlParseTest(){
 
         UserDatabase testDatabase = new UserDatabase("users.txt");
+        Account testAccount = Account.pseudoLogin("emil_lopez@dlsu.edu.ph", testDatabase);
         try {
-            StructureParser testParse = new StructureParser("test.xml", testDatabase);
+            StructureHandler testParse = new StructureHandler("test.xml", testDatabase);
+            System.out.println(testParse.addReservation("St. La Salle Hall", 1, "LS206", 2, 30, 1999, 10, 30,testAccount));
             ArrayList<Building> result = testParse.parse();
 
         } catch (ParserConfigurationException e) {
@@ -21,6 +26,10 @@ public class Tests {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
             e.printStackTrace();
         }
 
