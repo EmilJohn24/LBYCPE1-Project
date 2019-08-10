ba2c423592f2ed2e7a48ced19eca30f8214f9a25
Emil John Lopez <emiljohn24@gmail.com>
2019-08-10 22:02:52 +0800

    Turned to static class

--- src/Server/Transaction/Manager.java
+++ src/Server/Transaction/Manager.java
@@ -1,14 +1,20 @@
 package Server.Transaction;
 
-public class Manager {
-    private UserDatabase database;
+import Server.Structures.StructureParser;
 
+//central manager of every operation
+//will be set as a static class
+public class Manager {
+
+    private static UserDatabase database;
+    private static StructureParser parser;
     public Account getAccountLogin(String username,
                                    String password){
         return Account.login(username, password, database);
     }
 
-    Manager(String loginFile){
-        database = new UserDatabase(loginFile);
+    static {
+        database = new UserDatabase("users.txt");
+
     }
 }
