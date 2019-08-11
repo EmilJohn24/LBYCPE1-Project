2922d5d3795f00b17b74c199d9bc1b7a9aa60216
Emil John Lopez <emiljohn24@gmail.com>
2019-08-11 10:57:58 +0800

    Added request messages in comments

--- src/Client/ConnectorSocket.java
+++ src/Client/ConnectorSocket.java
@@ -5,7 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+//communicates with the server. For now, it simply displays them as messages.
+//Request Format:
+//[REQUEST_TYPE]:PARAMS
+//Normal Types:
+//LOGIN:[username],[password]
+//GET_ROOM_DATA
+//RESERVE:[SESSIONID],[BUILDING NAME],[FLOOR],[ROOM]
 
+
+//Returnable Messages:
+//ERROR_RESERVE
+//LOGIN_SUCCESS:[SESSIONID]
+//LOGIN_FAILED
+//INVALID_CREDENTIALS
+//RANDOM_NETWORK_ERROR
+//ROOM_NOT_AVAILABLE
 public class ConnectorSocket extends Socket {
     private String _hostname;
     private int _port;
@@ -30,6 +45,12 @@ public class ConnectorSocket extends Socket {
 
         }
     }
+
+    public String process(String request){
+        return null;
+    }
+
+
 
     public void sendRequest(String requestStr){
         output.println(requestStr);
