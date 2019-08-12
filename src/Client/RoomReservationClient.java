1d5cde6a432de6e138c0f982982650e4a60279f9
Emil John Lopez <emiljohn24@gmail.com>
2019-08-13 07:52:36 +0800

    Changed initial object to login

--- src/Client/RoomReservationClient.java
+++ src/Client/RoomReservationClient.java
@@ -1,24 +1,12 @@
 package Client;
 
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerConfigurationException;
+import Client.Login.Login;
 import java.io.IOException;
 
 public class RoomReservationClient {
     public static void main(String[] args) {
-        try {
-            Client client = new Client("127.0.0.1", 4400);
-            client.loadBuildingGUI();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
+
+        Login.runner(args);
+
     }
 }
