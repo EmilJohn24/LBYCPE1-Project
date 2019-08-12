77455a718d279cb9f8a104136d5dd13bcf6720a8
Emil John Lopez <emiljohn24@gmail.com>
2019-08-12 15:32:37 +0800

    Changed exception catching levels

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -9,20 +9,14 @@ public class Client {
         System.out.println(message);
     }
 
-    public Client(String hostname, int port){
-        try {
-            _socket = new ConnectorSocket(hostname, port);
-        }
-        catch (IOException e){
-            log("Error establishing connection. Error: " + e.getMessage());
-            e.printStackTrace();
-        }
+    public Client(String hostname, int port) throws IOException {
+        _socket = new ConnectorSocket(hostname, port);
+
     }
 
-    public void sendRequest(String requestStr) throws Exception {
-        if (_socket.isConnected())
-            _socket.sendRequest(requestStr);
-        else throw new Exception("Error connecting to server");
+    public void sendRequest(String requestStr) {
+        _socket.sendRequest(requestStr);
+
     }
 
     public String getResponse(){
