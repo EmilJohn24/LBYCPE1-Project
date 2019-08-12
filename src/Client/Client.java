91af4e95dc17b588afc87926de57d3ccbbb89a8b
Emil John Lopez <emiljohn24@gmail.com>
2019-08-13 00:48:13 +0800

    Added GUI Loader and Connector

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,21 +1,73 @@
 package Client;
 
-import java.io.IOException;
+import Client.Graphics.BuildingDisplay;
+import Server.Structures.Building;
+import Server.Structures.StructureHandler;
+import Server.Transaction.Account;
+import Server.Transaction.UserDatabase;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Client {
     private ConnectorSocket _socket;
-
+    private StructureHandler handler;
+    private UserDatabase localUserCopy;
+    private ArrayList<Building> buildings;
+    private int sessionID; //TODO: Implement proper login system
+    //GUI area
     public void log(String message){
         System.out.println(message);
     }
 
     public Client(String hostname, int port) throws IOException {
         _socket = new ConnectorSocket(hostname, port);
+        localUserCopy = new UserDatabase("users.txt"); //TODO: Lopez: delocalize users.txt access in future update
+                                                                // I recommend creating a function for loading this that requests password-less database transfer
 
     }
 
     public void sendRequest(String requestStr) {
         _socket.sendRequest(requestStr);
+    }
+
+    public void loadBuildings() throws SAXException, ParserConfigurationException, TransformerConfigurationException, IOException {
+        if (handler == null) loadRoomData();
+        buildings = handler.parse();
+    }
+
+    public void loadBuildingGUI() throws SAXException, ParserConfigurationException, TransformerConfigurationException, IOException {
+        if (buildings == null) loadBuildings();
+
+        GraphicClientEndConnector.connectClient(this);
+    }
+
+    public void loadRoomData() throws IOException, SAXException, ParserConfigurationException, TransformerConfigurationException {
+        sendRequest("GET_ROOM_DATA");
+        String response = getResponse();
+        String[] responseComponents = response.split(":");
+        String id = responseComponents[0];
+        String data;
+        if (id.equals("ROOM_DATA")){
+            data = responseComponents[1];
+            BufferedWriter tempWriter = new BufferedWriter(new FileWriter("temp.xml"));
+            tempWriter.write(data);
+            tempWriter.close();
+            handler = new StructureHandler("temp.xml", localUserCopy);
+
+        }
+
+        else if (id.equals("ERROR_SENDING_FILE")){
+            log("Well fuck");
+            System.exit(0);
+        }
+        else if (id == null){
+            log("Null string bois");
+        }
+
+
 
     }
 
--- src/Client/RoomReservationClient.java
+++ src/Client/RoomReservationClient.java
@@ -1,12 +1,23 @@
 package Client;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
 import java.io.IOException;
 
 public class RoomReservationClient {
     public static void main(String[] args) {
         try {
             Client client = new Client("127.0.0.1", 4400);
+            client.loadBuildingGUI();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
             e.printStackTrace();
         }
     }
