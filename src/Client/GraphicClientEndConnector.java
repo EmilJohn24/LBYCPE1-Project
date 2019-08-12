c2a3dccb06b79e3fcee61e127a3fa905db44316b
Emil John Lopez <emiljohn24@gmail.com>
2019-08-13 07:49:41 +0800

    Connected graphics to various aspects of client-side

--- src/Client/GraphicClientEndConnector.java
+++ src/Client/GraphicClientEndConnector.java
@@ -1,10 +1,16 @@
 package Client;
 
 import Client.Graphics.BuildingDisplay;
+import Client.Picker.Picker;
 import Server.Structures.Building;
 import Server.Structures.Floor;
 import Server.Structures.Room;
+import org.xml.sax.SAXException;
 
+import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -15,16 +21,93 @@ public  class GraphicClientEndConnector {
     private static Building currentBuilding;
     private static int currentFloor;
     private static Room currentRoom;
-    private static Calendar currentDate;
     private static BuildingDisplay buildingGUI;
-
+    private static Picker picker;
     public static void connectClient(Client c){
         topClient = c;
     }
+    private static int month;
+    private static int day;
+    private static int year;
+    private static int hour;
+    private static int minute;
 
+    public static Integer[] getDateInArrayForm(){
+        Integer[] date = new Integer[5];
+        date[0] = month;
+        date[1] = day;
+        date[2] = year;
+        date[3] = hour;
+        date[4] = minute;
+        return date;
+    }
+
+
+    public static void changeTime(int month, int day, int year, int hour, int minute){
+        GraphicClientEndConnector.month = month;
+        GraphicClientEndConnector.day = day;
+        GraphicClientEndConnector.year = year;
+        GraphicClientEndConnector.hour = hour;
+        GraphicClientEndConnector.minute = minute;
+    }
+
+
+
+    public static void processReservation(){
+        String response = topClient.sendReservationRequest(currentBuilding.getName(), currentFloor, currentRoom.getName(), month, day, year, hour, minute);
+        switch(response){
+            case "RESERVATION_COMPLETE":
+                JOptionPane.showMessageDialog(null, "Reservation successful");
+                RoomReservationClient.main(null);
+            case "SLOT_NOT_FOUND":
+                JOptionPane.showMessageDialog(null, "Please select an existing slot");
+                return;
+            case "ROOM_NOT_AVAILABLE":
+                JOptionPane.showMessageDialog(null, "This room is unavailable");
+                return;
+
+
+
+        }
+    }
     public static void loadUpBuildingGraphics(ArrayList<Building> buildings){
         buildingGUI.loadUpGraphics(buildings);
         buildingGUI.start();
+    }
+
+    public static void putFloorsInPicker(){
+        for (Floor f : currentBuilding.getFloors()){
+            picker.addChoice(f.getFloorCount());
+        }
+    }
+
+    public static void runPicker(){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                picker.setVisible(true);
+                picker.clear();
+                if (currentBuilding != null){
+                    putFloorsInPicker();
+                }
+            };
+        });
+
+
+    }
+
+    public static void runClient(){
+        try {
+            topClient.loadBuildingGUI();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
@@ -32,13 +115,14 @@ public  class GraphicClientEndConnector {
         return topClient;
     }
 
-    private static void displayNewFloor(int floor){
+    public static void displayNewFloor(int floor){
         buildingGUI.getNav().loadFloor(floor);
     }
 
 
     static{
         buildingGUI = new BuildingDisplay();
+        picker = new Picker();
     }
 
     public static Building getCurrentBuilding() {
@@ -54,8 +138,8 @@ public  class GraphicClientEndConnector {
     }
 
     public static void setCurrentFloor(int currentFloor) {
-        displayNewFloor(currentFloor);
         GraphicClientEndConnector.currentFloor = currentFloor;
+        displayNewFloor(currentFloor);
     }
 
     public static Room getCurrentRoom() {
@@ -66,11 +150,5 @@ public  class GraphicClientEndConnector {
         GraphicClientEndConnector.currentRoom = currentRoom;
     }
 
-    public static Calendar getCurrentDate() {
-        return currentDate;
-    }
 
-    public static void setCurrentDate(Calendar currentDate) {
-        GraphicClientEndConnector.currentDate = currentDate;
-    }
 }
