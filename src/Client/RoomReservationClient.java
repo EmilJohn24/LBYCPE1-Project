847966c667713d4c98e607e77036e017ec29fda7
Emil John Lopez <emiljohn24@gmail.com>
2019-08-12 15:33:13 +0800

    Exception handling for client

--- src/Client/RoomReservationClient.java
+++ src/Client/RoomReservationClient.java
@@ -1,7 +1,13 @@
 package Client;
 
+import java.io.IOException;
+
 public class RoomReservationClient {
     public static void main(String[] args) {
-        Client client = new Client("127.0.0.1", 4400);
+        try {
+            Client client = new Client("127.0.0.1", 4400);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
